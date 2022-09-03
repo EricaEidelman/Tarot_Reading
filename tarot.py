6cf8dd31ade258048d383f4f18baacd3ef8201c1
E E <e10e31e@gmail.com>
2022-09-02 22:33:33 -0400

    Added function to shuffle deck and select three cards for reading

--- tarot.py
+++ tarot.py
@@ -1,3 +1,6 @@
+# Imports
+import random
+
 # Write out the cards
 major_arcana = ["The Fool", "The Magician", "The High Priestess", "The Empress", "The Emperor", "The Hierophant",
  "The Lovers", "The Chariot", "Strength", "The Hermit", "Wheel of Fortune", "Justice", "The Hanged Man", "Death", 
@@ -18,4 +21,36 @@ suit_deck = suit_card_combine(suits, cards)
 
 complete_deck = major_arcana + suit_deck
 
-print(complete_deck)
+# Function to shuffle the complete deck
+def shuffle_deck(deck):
+    i = 78
+    shuffled_deck = []
+    while i > 0:
+        shuffled_deck.append(deck[random.randrange(0,i)])
+        i -= 1
+    return shuffled_deck
+
+# Function to select three cards
+def card_selector(shuffled_deck):
+    i = 78
+    selected_cards = []
+    while i > 75:
+        selected_cards.append(shuffled_deck[random.randrange(0,i)])
+        i -= 1
+    return(selected_cards)
+
+# Function to announce selected cards
+def card_teller(selected_deck):
+    print("Your past card is " + selected_deck[0])
+    print("Your present card is " + selected_deck[1])
+    print("Your future card is " + selected_deck[2])
+
+input("Hello, are you here for a tarot card reading? Let's get started. What is your name?")
+print("Ok, now I will shuffle the cards.")
+
+shuffled_deck = shuffle_deck(complete_deck)
+
+print("The cards are now shuffled. I will now select three cards to represent your past, present, and future.")
+
+selected_cards = card_selector(shuffled_deck)
+card_teller(selected_cards)
